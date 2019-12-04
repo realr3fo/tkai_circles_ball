9dde103e29b9beebfad31f7c29f4aad8658037d5
refoilmiya <refo.ilmiya@gmail.com>
2019-12-04 14:41:37 +0700

    base for users api and contacts

---
+++ controllers/contactsControllers.go
@@ -0,0 +1,41 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+	"tkai/models"
+	u "tkai/utils"
+)
+
+var CreateContact = func(w http.ResponseWriter, r *http.Request) {
+
+	user := r.Context().Value("user") . (uint) //Grab the id of the user that send the request
+	contact := &models.Contact{}
+
+	err := json.NewDecoder(r.Body).Decode(contact)
+	if err != nil {
+		u.Respond(w, u.Message(false, "Error while decoding request body"))
+		return
+	}
+
+	contact.UserId = user
+	resp := contact.Create()
+	u.Respond(w, resp)
+}
+
+var GetContactsFor = func(w http.ResponseWriter, r *http.Request) {
+	//params := mux.Vars(r)
+	id := r.Context().Value("user") . (uint)
+	//id, err := strconv.Atoi(params["id"])
+	//if err != nil {
+	if id == 0 {
+		//The passed path parameter is not an integer
+		u.Respond(w, u.Message(false, "There was an error in your request"))
+		return
+	}
+
+	data := models.GetContacts(uint(id))
+	resp := u.Message(true, "success")
+	resp["data"] = data
+	u.Respond(w, resp)
+}
